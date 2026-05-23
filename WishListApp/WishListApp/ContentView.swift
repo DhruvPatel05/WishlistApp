ec1428eee31aaece12539aa81a8c48d5f72975c6
Dhruv Patel <dhruvpatel913@gmail.com>
2026-05-22 17:02:47 -0700

    Initial Commit

---
+++ WishListApp/WishListApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WishListApp
+//
+//  Created by Dhruv Patel on 22/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WishListApp/WishListApp/WishListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishListAppApp.swift
+//  WishListApp
+//
+//  Created by Dhruv Patel on 22/05/26.
+//
+
+import SwiftUI
+
+@main
+struct WishListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
